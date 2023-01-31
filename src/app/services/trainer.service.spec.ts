e22d50781590b1d8af5059947079e8286d136b89
Skryseten <skryseth@gmail.com>
2023-01-31 07:35:18 +0100

    Feat: added auth guard

--- src/app/services/trainer.service.spec.ts
+++ src/app/services/trainer.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserService } from './user.service';
+import { TrainerService } from './trainer.service';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('TrainerService', () => {
+  let service: TrainerService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    service = TestBed.inject(TrainerService);
   });
 
   it('should be created', () => {
